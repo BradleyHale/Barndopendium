43c95f91cc9b2a6dd4ffd23ad6ef9210656d74ff
BradleyHale <bradh2018@gmail.com>
2023-07-03 16:36:08 -0500

    attempting to resolve electron issues

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+// NOTE: THIS VERSION D O E S  N O T COMPATIBLE WITH ELECTRON
 "use strict";
 require("dotenv").config();
-const path = require('path');
-// const { app, BrowserWindow } = require('electron'); | will implement at the end
-// TODO LIST: parse req.query
+const { app, BrowserWindow } = require('electron');
 const express = require("express");
-const app = express();
-app.use(express.static("public", {
+const server = express();
+server.use(express.static("public", {
    index: "index.html",
    extensions: ['html']
 }));
 
-// app.use(express.static("public", {index: "index.html", extensions: ["html"]}));
+const createWindow = () => {
+   const win = new BrowserWindow({
+      width: 800,
+      height: 600
+   })
+   win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+   createWindow();
+})
+
+server.use(express.static("public", {index: "index.html", extensions: ["html"]}));
 
 // require validators
 const planValidator = require("./Validators/planValidator");
@@ -18,27 +29,29 @@ const planValidator = require("./Validators/planValidator");
 // require controller
 const planController = require("./Controllers/planController");
 
+server.set("view engine", "ejs");
+
 // endpoints ------------------------------------
 
 
 // // index GET: test to see if the server is working on localhost
- app.get("/", (req, res) => {
+ server.get("/", (req, res) => {
     console.log("this is a test");
 });
 
  // GET - retrieve data from database
- app.get("/api/search", planController.searchByOp);
+ server.get("/api/search", planController.searchByOp);
 
  // POST - insert new plans into database
-// app.post("/api/insert",
+// server.post("/api/insert",
 //    planValidator.validateRegisterBody,
 //    planController.addPlan
 // );
 
 const {PORT} = process.env;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
    console.log(`Listening on http://localhost:${PORT}`)
 })
 module.exports = {
-   app
+   server
 };
